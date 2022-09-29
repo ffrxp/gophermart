19daa141a0673e75fe24a555a1feb477c086661f
ffrxp <ldm.pubmail@yandex.ru>
2022-09-30 02:55:36 +0300

    version #1

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,3 +1,29 @@
 package main
 
-func main() {}
+import (
+	"github.com/ffrxp/gophermart/internal/app"
+	"github.com/ffrxp/gophermart/internal/common"
+	"github.com/ffrxp/gophermart/internal/handlers"
+	"github.com/ffrxp/gophermart/internal/storage"
+	"github.com/rs/zerolog/log"
+	"net/http"
+)
+
+func main() {
+	log.Info().Msg("Start gophermart")
+	config := common.InitConfig()
+	if config.DatabaseURI == "" {
+		log.Panic().Msg("Database URI is empty.")
+		//panic("Database URI is empty. Please fill it.")
+	}
+	appStorage, err := storage.NewDatabaseStorage(config.DatabaseURI)
+	if err == nil {
+		defer appStorage.Close()
+		ga := app.GophermartApp{Storage: appStorage,
+			DatabaseURI:       config.DatabaseURI,
+			AccrualSystemAddr: config.AccrualSystemAddr}
+		log.Info().Msgf("Start server. Address:'%s'|Database URI:'%s'|Accrual address:'%s'",
+			config.RunAddress, config.DatabaseURI, config.AccrualSystemAddr)
+		log.Panic().Msg(http.ListenAndServe(config.RunAddress, handlers.NewRouter(&ga)).Error())
+	}
+}
---
+++ internal/common/common.go
@@ -0,0 +1,72 @@
+package common
+
+import (
+	"crypto/sha256"
+	"flag"
+	"fmt"
+	"os"
+)
+
+const defaultRunAddress = ":8080"
+
+type Config struct {
+	RunAddress        string
+	DatabaseURI       string
+	AccrualSystemAddr string
+}
+
+func InitConfig() *Config {
+	var conf Config
+
+	defRunAddress, ok := os.LookupEnv("RUN_ADDRESS")
+	if !ok || defRunAddress == "" {
+		defRunAddress = defaultRunAddress
+	}
+	defDatabaseURI, ok := os.LookupEnv("DATABASE_URI")
+	if !ok {
+		defDatabaseURI = ""
+	}
+	defAccrualSystemAddr, ok := os.LookupEnv("ACCRUAL_SYSTEM_ADDRESS")
+	if !ok {
+		defAccrualSystemAddr = ""
+	}
+
+	flag.StringVar(&(conf.RunAddress), "a", defRunAddress, "Address and port of starting service")
+	flag.StringVar(&(conf.DatabaseURI), "d", defDatabaseURI, "Database URI")
+	flag.StringVar(&(conf.AccrualSystemAddr), "r", defAccrualSystemAddr, "Address of accrual system")
+	flag.Parse()
+
+	return &conf
+}
+
+func GenHashForRawPassword(pwd, salt string) []byte {
+	pwdWithSalt := fmt.Sprintf("%s%s", pwd, salt)
+
+	h := sha256.New()
+	h.Write([]byte(pwdWithSalt))
+	res := h.Sum(nil)
+	return res
+}
+
+// Реализация алгоритма Луна из Википедии. Если будет время - реализую самостоятельно
+func IsValidByLuhnAlg(number int64) bool {
+	return (number%10+checksum(number/10))%10 == 0
+}
+func checksum(number int64) int64 {
+	var luhn int64
+
+	for i := 0; number > 0; i++ {
+		cur := number % 10
+
+		if i%2 == 0 { // even
+			cur = cur * 2
+			if cur > 9 {
+				cur = cur%10 + cur/10
+			}
+		}
+
+		luhn += cur
+		number = number / 10
+	}
+	return luhn % 10
+}
